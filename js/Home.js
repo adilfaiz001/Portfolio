9395ae9d238a719b8788929f4fb93d094c3187dc
Adil Faiz <adilfaiz01@live.com>
2017-01-10 01:54:22 +0530

    Starting Web Development further

--- js/Home.js
+++ js/Home.js
@@ -1,5 +1,7 @@
 
+/*Top Menu down scroll*/
 window.onscroll = function() {navbarScroll()};
+
 
 function navbarScroll() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -10,3 +12,19 @@ function navbarScroll() {
         document.getElementById("navbar").style.transition="transform 0.4s";
     }
 }
+/*.......................*/
+
+/*Slide Left Menu Button*/
+document.getElementById('sl-menu-close').onclick=function()
+{
+  document.getElementById('slide-left-menu').style.transform="translateX(-27em)";
+  document.getElementById('slide-left-menu').style.transition="transform 1s";
+};
+
+$(document).ready(function(){
+  $('#c-hamburger,#side-menu').click(function(){
+    $('#slide-left-menu').css({"transform":"translateX(-4em)"});
+    $('#slide-left-menu').css({"transition":"transform 1s"});
+  });
+});
+/*.........................*/
