1dc122b936069d8c266a13c3e5d275b76270c4bb
Adil Faiz <adilfaiz01@live.com>
2018-09-05 00:10:35 +0530

    js

---
+++ js/project.js
@@ -0,0 +1,12 @@
+
+
+
+var all_project_template,featured_project_template,category_template,timeline_template;
+
+
+
+
+$(document).ready(function()
+{
+
+});
---
+++ js/project_data.js
@@ -0,0 +1,77 @@
+
+
+
+var projects =  {
+        all_projects:[
+                      {
+                        name : "Project Velvet",
+                        des : "",
+                        thumbnail: "",
+                        img  :
+                                {
+                                  img1 : "",
+                                  img2 : "",
+                                  img3 : "",
+                                },
+
+                        url : "",
+                        git_url : "",
+                      },
+                      {
+                        name: "Project ORVM",
+                        des : "",
+                        img :
+                                {
+                                  img1: "",
+                                },
+                        url : "",
+                        git_url : "",
+
+                      }
+                      {
+                        name : "Handwritten Digit Recognizer",
+                        des : "",
+                        img : {
+
+                        },
+                        url : "",
+                        git_url : "",
+                      }
+                      {
+                        name : "Website Portfolio",
+                        des: "",
+                        img : {
+
+                        },
+                        url : "",
+                        git_url : "",
+                      }
+                      ],
+
+
+
+    featured_projects:[
+                       {
+
+                       }
+                      ],
+
+
+
+    category_projects:{
+                      deeplearning:[],
+                      machine_learning:[],
+                      web_dev:[],
+                      iot:[],
+                      },
+
+
+
+
+    timeline_projects:{
+                      t_16:[],
+                      t_17:[],
+                      t_18:[],
+                      }
+
+                };
